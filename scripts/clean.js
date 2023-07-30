2662ecc6d5dbec6323bdebe81dc40128e991f302
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-07-30 11:59:03 -0500

    updates

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,15 @@
 const sh = require('shelljs');
 const upath = require('upath');
+const fs = require('fs');
 
 const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
+// Clean the entire 'docs' folder
+sh.rm('-rf', `${destPath}/*`);
 
+// Create empty 'css' and 'js' folders inside the 'docs' folder
+const cssFolder = upath.join(destPath, 'css');
+const jsFolder = upath.join(destPath, 'js');
+
+fs.mkdirSync(cssFolder);
+fs.mkdirSync(jsFolder);
