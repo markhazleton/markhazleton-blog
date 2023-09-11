f1c5ddbde4b61cce4e1c81164d41336eb44a31da
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-09-11 14:22:00 -0500

    Update Admin to edit/save PROJECTS.JSON file

---
+++ WebAdmin/mwhWebAdmin/Pages/ProjectEdit.cshtml.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using mwhWebAdmin.Project;
+
+namespace mwhWebAdmin.Pages;
+
+public class ProjectEditModel : PageModel
+{
+private readonly ProjectService _projectService;
+private readonly IWebHostEnvironment _webHostEnvironment;
+
+public ProjectEditModel(ProjectService projectService, IWebHostEnvironment webHostEnvironment)
+{
+    _projectService = projectService;
+    _webHostEnvironment = webHostEnvironment;
+}
+
+[BindProperty]
+public ProjectModel Project { get; set; }
+
+public List<string> AvailableImages { get; set; }
+
+public IActionResult OnGet(int id)
+{
+    Project = _projectService.GetProjectById(id);
+
+    if (Project == null)
+    {
+        return NotFound();
+    }
+
+        // Get the list of available images from the virtual directory
+        var imagesDirectory = Path.GetFullPath(Path.Combine("..", "..", "src", "assets", "img"));
+        var rootPath = Path.GetFullPath(Path.Combine("..", "..", "src"));
+        var images = Directory.GetFiles(imagesDirectory, "*.*", SearchOption.TopDirectoryOnly)
+            .Select(imagePath => imagePath.Replace(rootPath, "").Replace("\\", "/").TrimStart('/'))
+            .ToList();
+
+
+        AvailableImages = images;
+
+        return Page();
+}
+
+public IActionResult OnPost()
+{
+    // Update the project details
+    _projectService.UpdateProject(Project);
+
+    // Redirect to a page displaying the updated project or a list of projects
+    return RedirectToPage("/Projects");
+}
+}
--- WebAdmin/mwhWebAdmin/Project/ProjectModel.cs
+++ WebAdmin/mwhWebAdmin/Project/ProjectModel.cs
@@ -1,24 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace mwhWebAdmin.Project;
 
 public class ProjectService
 {
     private readonly string _jsonFilePath;
+    private List<ProjectModel> _projects;
 
     public ProjectService(string jsonFilePath)
     {
         _jsonFilePath = jsonFilePath;
+        LoadProjects();
     }
+
+    private void LoadProjects()
+    {
+        string json = File.ReadAllText(_jsonFilePath);
+        _projects = JsonSerializer.Deserialize<List<ProjectModel>>(json);
+    }
+
 
     public List<ProjectModel> GetProjects()
     {
         string json = File.ReadAllText(_jsonFilePath);
-        List<ProjectModel> projects = JsonSerializer.Deserialize<List<ProjectModel>>(json);
-        return projects;
+        _projects = JsonSerializer.Deserialize<List<ProjectModel>>(json);
+        return _projects;
     }
 
     public void SaveProjects(List<ProjectModel> projects)
     {
-        JsonSerializerOptions options = new JsonSerializerOptions
+        JsonSerializerOptions options = new()
         {
             WriteIndented = true
         };
@@ -26,10 +37,51 @@ public class ProjectService
         string json = JsonSerializer.Serialize(projects, options);
         File.WriteAllText(_jsonFilePath, json);
     }
+
+    public ProjectModel GetProjectById(int id)
+    {
+        return _projects.FirstOrDefault(p => p.Id == id);
+    }
+
+    public void UpdateProject(ProjectModel updatedProject)
+    {
+        if (updatedProject == null)
+        {
+            throw new ArgumentNullException(nameof(updatedProject));
+        }
+
+        var existingProject = _projects.FirstOrDefault(p => p.Id == updatedProject.Id);
+        if (existingProject == null)
+        {
+            throw new InvalidOperationException("Project not found.");
+        }
+
+        // Update the project details
+        existingProject.Title = updatedProject.Title;
+        existingProject.Description = updatedProject.Description;
+        existingProject.Link = updatedProject.Link;
+
+        // Save the updated projects back to the JSON file
+        SaveProjects();
+    }
+
+    private void SaveProjects()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        string json = JsonSerializer.Serialize(_projects, options);
+        File.WriteAllText(_jsonFilePath, json);
+    }
 }
 
 public class ProjectModel
 {
+    [Key]
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
     [JsonPropertyName("image")]
     public string Image { get; set; }
     [JsonPropertyName("p")]
