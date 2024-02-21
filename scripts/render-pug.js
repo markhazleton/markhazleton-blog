2861a91c7d250df37b783583cd5bed2d90c775c7
Mark Hazleton <mark@markhazleton.com>
2024-02-21 06:33:32 -0600

    updates start post on wichitasewer.com

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -15,7 +15,6 @@ module.exports = async function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\//, 'docs/').replace(/\.pug$/, '.html');
     const srcPath = upath.resolve(upath.dirname(__filename), '../src');
 
-    console.log(`### INFO: Rendering ${filePath} to ${destPath}`);
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
