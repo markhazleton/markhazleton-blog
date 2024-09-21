6870e0c157efa05693907937c1eb9b2ce6bc75e7
Mark Hazleton <mark@markhazleton.com>
2024-09-20 19:43:16 -0500

    update migration article

--- scripts/start.js
+++ scripts/start.js
@@ -1,14 +1,11 @@
 const concurrently = require("concurrently");
 const upath = require("upath");
-
 const browserSyncPath = upath.resolve(
     upath.dirname(__filename),
     "../node_modules/.bin/browser-sync.cmd"
 );
-
 // Using PowerShell command in the correct format
 const browserSyncCommand = `powershell -Command "& '${browserSyncPath}' --reload-delay 2000 --reload-debounce 2000 docs -w --no-online --https --key 'C:\\Program Files\\OpenSSL-Win64\\bin\\localhost.key' --cert 'C:\\Program Files\\OpenSSL-Win64\\bin\\localhost.crt'"`;
-
 concurrently(
     [
         {
@@ -28,11 +25,9 @@ concurrently(
         restartTries: 3, // Optional: Set restart attempts if failure occurs
     }
 );
-
 function success() {
     console.log("Success");
 }
-
 function failure() {
     console.log("Failure");
 }
