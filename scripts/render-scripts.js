ab8760bfa3a8044212a1ff34ec99ca460890a4d2
Mark Hazleton <mark@markhazleton.com>
2024-09-29 14:04:27 -0500

    update github article add more language for prism.js

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -15,22 +15,30 @@ module.exports = async function renderScripts() {
     // Path to Bootstrap JS in node_modules (adjust according to your Bootstrap version)
     const bootstrapJSPath = upath.resolve(upath.dirname(__filename), '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
 
-    // Path to PrismJS in node_modules (adjust according to your PrismJS setup)
+    // Path to PrismJS and the YAML language component in node_modules
     const prismJSPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/prism.js');
+    const prismYAMLPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-yaml.min.js');
+    const prismXMLPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-xml-doc.min.js');
+    const prismPUGPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-pug.min.js');
+    const prismCSHARPPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-csharp.min.js');
 
     // Paths for scripts.js and its destination
     const sourcePathScriptsJS = upath.resolve(upath.dirname(__filename), '../src/js/scripts.js');
     const destPathScriptsJS = upath.resolve(upath.dirname(__filename), '../docs/js/scripts.js');
 
-    // Read the Bootstrap JS, PrismJS, and your scripts.js content
-    const [bootstrapJS, prismJS, scriptsJS] = await Promise.all([
+    // Read the Bootstrap JS, PrismJS, Prism YAML, and your scripts.js content
+    const [bootstrapJS, prismJS, prismYAML, prismXML, prismPUG, prismCSHARP, scriptsJS] = await Promise.all([
         fs.readFile(bootstrapJSPath, 'utf8'),
         fs.readFile(prismJSPath, 'utf8'),
+        fs.readFile(prismYAMLPath, 'utf8'),
+        fs.readFile(prismXMLPath, 'utf8'),
+        fs.readFile(prismPUGPath, 'utf8'),
+        fs.readFile(prismCSHARPPath, 'utf8'),
         fs.readFile(sourcePathScriptsJS, 'utf8')
     ]);
 
-    // Combine Bootstrap JS, PrismJS, and your scripts.js
-    const combinedScripts = bootstrapJS + '\n' + prismJS + '\n' + scriptsJS;
+    // Combine Bootstrap JS, PrismJS, Prism YAML, and your scripts.js
+    const combinedScripts = bootstrapJS + '\n' + prismJS + '\n' + prismYAML + '\n' + prismXML + '\n' + prismPUG + '\n' + prismCSHARP + '\n' + scriptsJS;
 
     // Include copyright notice
     const copyrightNotice = `/*!
