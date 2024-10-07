cd2d03fc64c14f865abecc364536a039585c31f1
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-10-07 09:34:46 -0500

    update to accordion global settings

--- WebAdmin/mwhWebAdmin/Article/ArticleModel.cs
+++ WebAdmin/mwhWebAdmin/Article/ArticleModel.cs
@@ -1,38 +1,71 @@
 namespace mwhWebAdmin.Models;
 
+/// <summary>
+/// Represents an article model with various properties such as Id, Section, Slug, Name, Content, Description, Image Source, Last Modified date, and Change Frequency.
+/// </summary>
 public class ArticleModel
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArticleModel"/> class with default values for Slug and ImgSrc.
+    /// </summary>
     public ArticleModel()
     {
         Slug = "tbd";
         ImgSrc = "assets/img/ArgostoliGreeceBeach.jpg";
     }
+
+    /// <summary>
+    /// Gets or sets the unique identifier for the article.
+    /// </summary>
     [JsonPropertyName("id")]
     public int Id { get; set; }
-
+    ///
+    /// <summary>
+    /// Gets or sets the section to which the article belongs.
+    /// </summary>
     [JsonPropertyName("Section")]
     public string Section { get; set; }
 
+    /// <summary>
+    /// Gets or sets the slug (URL-friendly string) for the article.
+    /// </summary>
     [JsonPropertyName("slug")]
     public string Slug { get; set; }
 
+    /// <summary>
+    /// Gets or sets the name of the article.
+    /// </summary>
     [JsonPropertyName("name")]
     public string Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets the content of the article.
+    /// </summary>
     [JsonPropertyName("content")]
     public string ArticleContent { get; set; }
-
+    ///
+    /// <summary>
+    /// Gets or sets the description of the article.
+    /// </summary>
     [JsonPropertyName("description")]
     public string Description { get; set; }
-
+    ///
+    /// <summary>
+    /// Gets or sets the image source URL for the article.
+    /// </summary>
     [JsonPropertyName("img_src")]
     public string ImgSrc { get; set; }
 
+    /// <summary>
+    /// Gets or sets the last modified date of the article.
+    /// </summary>
     [JsonPropertyName("lastmod")]
     public string LastModified { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
-
+    ///
+    /// <summary>
+    /// Gets or sets the change frequency of the article.
+    /// </summary>
     [JsonPropertyName("changefreq")]
     public string ChangeFrequency { get; set; } = "monthly";
-
 }
 
