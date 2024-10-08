d14bf26a661de43e646d963adb630a0dd3ff1f3e
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-10-08 11:34:58 -0500

    reorganize article categories

---
+++ WebAdmin/mwhWebAdmin/Pages/ArticleModelHelpers.cs
@@ -0,0 +1,24 @@
+public static class ArticleModelHelpers
+{
+    public static List<string> Sections { get; } =
+    [
+        "Case Studies",
+        "ChatGPT",
+        "Blog Management",
+        "Data Science",
+        "Development",
+        "Personal Philosophy",
+        "Project Mechanics",
+        "PromptSpark"
+    ];
+    public static List<string> ChangeFrequency { get; } =
+    [
+        "always",
+        "hourly",
+        "daily",
+        "weekly",
+        "monthly",
+        "yearly",
+        "never"
+    ];
+}
