03d62fb86633c3137c8e8180cc500159dfdb3b94
Mark Hazleton <mark.hazleton@controlorigins.com>
2025-01-03 09:52:18 -0600

    update to add prism for scss and json

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -12,7 +12,7 @@ module.exports = async function renderScripts() {
 
     sh.cp('-R', sourcePath, destPath);
 
-    // Path to Bootstrap JS in node_modules (adjust according to your Bootstrap version)
+    // Path to Bootstrap JS in node_modules
     const bootstrapJSPath = upath.resolve(upath.dirname(__filename), '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
 
     // Path to PrismJS and the language components in node_modules
@@ -21,30 +21,41 @@ module.exports = async function renderScripts() {
     const prismXMLPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-xml-doc.min.js');
     const prismPUGPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-pug.min.js');
     const prismCSHARPPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-csharp.min.js');
-    const prismPythonPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-python.min.js'); // Add Python component
-    const prismBashPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-bash.min.js'); // Add Bash component
-    const prismTypeScriptPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-typescript.min.js'); // Add TypeScript component
+    const prismPythonPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-python.min.js');
+    const prismBashPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-bash.min.js');
+    const prismTypeScriptPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-typescript.min.js');
+    const prismSCSSPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-scss.min.js'); // SCSS
+    const prismJSONPath = upath.resolve(upath.dirname(__filename), '../node_modules/prismjs/components/prism-json.min.js'); // JSON
 
     // Paths for scripts.js and its destination
     const sourcePathScriptsJS = upath.resolve(upath.dirname(__filename), '../src/js/scripts.js');
     const destPathScriptsJS = upath.resolve(upath.dirname(__filename), '../docs/js/scripts.js');
 
-    // Read all JS files, including the new Bash and TypeScript components
-    const [bootstrapJS, prismJS, prismYAML, prismXML, prismPUG, prismCSHARP, prismPython, prismBash, prismTypeScript, scriptsJS] = await Promise.all([
+    // Read all JS files, including the new SCSS and JSON components
+    const [
+        bootstrapJS, prismJS, prismYAML, prismXML, prismPUG, prismCSHARP,
+        prismPython, prismBash, prismTypeScript, prismSCSS, prismJSON,
+        scriptsJS
+    ] = await Promise.all([
         fs.readFile(bootstrapJSPath, 'utf8'),
         fs.readFile(prismJSPath, 'utf8'),
         fs.readFile(prismYAMLPath, 'utf8'),
         fs.readFile(prismXMLPath, 'utf8'),
         fs.readFile(prismPUGPath, 'utf8'),
         fs.readFile(prismCSHARPPath, 'utf8'),
-        fs.readFile(prismPythonPath, 'utf8'), // Read Python component
-        fs.readFile(prismBashPath, 'utf8'), // Read Bash component
-        fs.readFile(prismTypeScriptPath, 'utf8'), // Read TypeScript component
+        fs.readFile(prismPythonPath, 'utf8'),
+        fs.readFile(prismBashPath, 'utf8'),
+        fs.readFile(prismTypeScriptPath, 'utf8'),
+        fs.readFile(prismSCSSPath, 'utf8'), // SCSS
+        fs.readFile(prismJSONPath, 'utf8'), // JSON
         fs.readFile(sourcePathScriptsJS, 'utf8')
     ]);
 
-    // Combine all scripts, including the new Bash and TypeScript components
-    const combinedScripts = bootstrapJS + '\n' + prismJS + '\n' + prismYAML + '\n' + prismXML + '\n' + prismPUG + '\n' + prismCSHARP + '\n' + prismPython + '\n' + prismBash + '\n' + prismTypeScript + '\n' + scriptsJS;
+    // Combine all scripts
+    const combinedScripts = [
+        bootstrapJS, prismJS, prismYAML, prismXML, prismPUG, prismCSHARP,
+        prismPython, prismBash, prismTypeScript, prismSCSS, prismJSON, scriptsJS
+    ].join('\n');
 
     // Include copyright notice
     const copyrightNotice = `/*!
