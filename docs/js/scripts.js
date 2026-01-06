3969812222d8b6ffdc999a1516ae9d5baa060ba7
Mark Hazleton <mark@markhazleton.com>
2026-01-05 19:00:47 -0600

    Update RSS and Sitemap Timestamps, Version Bump to 9.0.87
    - Updated lastBuildDate in rss.xml and articles-rss.xml to reflect new timestamp.
    - Adjusted lastmod timestamps in sitemap.xml and projects-rss.xml for consistency.
    - Incremented version number in package-lock.json and various HTML files to 9.0.87.
    - Updated references to modern-styles.css and scripts.js to version 9.0.87 across multiple HTML documents.

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Mark Hazleton Blog v9.0.84 (https://github.com/markhazleton/markhazleton-blog)
+* Start Bootstrap - Mark Hazleton Blog v9.0.86 (https://github.com/markhazleton/markhazleton-blog)
 * Copyright 2013-2026 Mark Hazleton (https://markhazleton.com)
 * Licensed under MIT (https://github.com/StartBootstrap/markhazleton-blog/blob/master/LICENSE)
 */
